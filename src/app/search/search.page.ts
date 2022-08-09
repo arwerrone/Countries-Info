b934712bbb4b410cfda1c206b985ef8d779aa060
Angelo Dikon Wong <78374435+arwerrone@users.noreply.github.com>
2022-08-09 18:28:07 -0300

    Country Service added

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {CountryData} from '../CountryData';
+
+interface Country{
+  name: string,
+  url: string,
+}
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
+
 export class SearchPage implements OnInit {
+
+  listOfCountries: Country[] = [];
+
 
   constructor() { }
 
   ngOnInit() {
+    this.listOfCountries = CountryData.data;
+
+  }
+
+  searchChanged(){
+    
   }
 
 }
